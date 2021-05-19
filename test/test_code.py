fae5a354e5afe47f0083569af1f5a59bc13c2958
matt <matthewharrison@gmail.com>
2021-05-19 16:10:59 -0600

    Update file name. Add decorator

--- test/test_code.py
+++ test/test_code.py
@@ -1,6 +1,18 @@
-import code
+import pyaction
 
+import pytest
+
+def skipit(name):
+    return pytest.mark.skipif(name not in dir(pyaction),               reason='missing')
+
+@skipit('parse')
 def test_parse():
     txt = '1,2,10'
-    res = code.parse(txt)
+    res = pyaction.parse(txt)
     assert res == [1,2,10]
+
+
+@skipit('sub')
+def test_sub():
+    res = pyaction.sub(1,2)
+    assert res == -1
